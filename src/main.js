7361e7c33c45e42914d543649d05ce9e169a25e3
hoangtrung1801 <hoangtrung1801.2003@gmail.com>
2021-09-05 13:14:17 +0700

    package app beta

--- src/main.js
+++ src/main.js
@@ -3,31 +3,6 @@ const Store = require('electron-store');
 const path = require("path");
 
 const store = new Store();
-store.set('data', [
-  {
-    id: '0',
-    title: 'Page 1',
-    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis cum deleniti aliquid repellat, maiores tempora cumque perspiciatis ea adipisci in dolore omnis magni. Deleniti sunt et laudantium sed vel necessitatibus.' 
-  },
-  {
-    id: '1',
-    title: 'Page 2',
-    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis cum deleniti aliquid repellat, maiores tempora cumque perspiciatis ea adipisci in dolore omnis magni. Deleniti sunt et laudantium sed vel necessitatibus.' 
-  }
-])
-
-let data = [
-  {
-    id: '0',
-    title: 'Page 1',
-    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis cum deleniti aliquid repellat, maiores tempora cumque perspiciatis ea adipisci in dolore omnis magni. Deleniti sunt et laudantium sed vel necessitatibus.' 
-  },
-  {
-    id: '1',
-    title: 'Page 2',
-    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis cum deleniti aliquid repellat, maiores tempora cumque perspiciatis ea adipisci in dolore omnis magni. Deleniti sunt et laudantium sed vel necessitatibus.' 
-  }
-]
 
 app.whenReady().then(() => {
   createWindow();
@@ -56,27 +31,3 @@ function createWindow() {
   // no relative path (if error then fix)
   win.loadFile("./public/index.html");
 }
-
-// IPCMain
-ipcMain.on('getData', (e) => {
-  e.returnValue = data;
-})
-
-let pageActiveId = '0' 
-ipcMain.on('pageActiveId:get', (e) => {
-  e.returnValue = pageActiveId;
-})
-
-ipcMain.on('pageActiveId:post', (e, arg) => {
-  pageActiveId = arg;
-})
-
-ipcMain.handle('page:put', (e, arg) => {
-  data = data.map(page => {
-    if(page.id === arg.id) {
-      return arg;
-    }
-    return page;
-  })
-  return data;
-})
